c7969844d801355ff8c279fd6dd5a16f994a2079
Maria13579 <patymsr3@gmail.com>
2021-06-04 09:29:27 -0600

    a

---
+++ User.Controller.php
@@ -0,0 +1,13 @@
+<?php
+	class User
+	{
+	  public function Login()
+	  {
+		echo "en login";
+	  }
+	  public function Delet()
+	  {
+		echo " en delet";
+	  }
+	}
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+   include('User.Controller.php');
+
+   if(isset($_GET['class'])&& isset($_GET['method']))	
+	{
+  	  $class= $_GET['class'];
+	  $method = $_GET['method'];
+	}
+	else
+	{
+	  echo "primera carga";
+	}
+	$c = new $class();
+	$c-> $method();  
+?>
